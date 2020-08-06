0931f896a2709fd1aa0167fce9567d16029f3c10
KolesarAndreja <kolesar.andreja@gmail.com>
2020-08-06 12:04:13 +0200

    new project made

---
+++ DAN_LI/ViewModel/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+namespace DAN_LI.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
